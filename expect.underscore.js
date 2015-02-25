4e6bead14ecae243a8d1dfc65153332d9135cf3d
Kim, Jang-hwan <janghwan@gmail.com>
2015-02-25 21:07:27 +0900

    initial commit.

---
+++ expect.underscore.js
@@ -0,0 +1,64 @@
+var expect  = require('expect.js'),
+    _       = require('lodash');
+
+/*
+ *
+ * Patch expect.js to extend underscore/lodash methods
+ *
+ */
+
+module.exports = {
+    extend: function(expect) {
+        // patch expect
+        var util = require('util'),
+        i = util.inspect;
+
+        // returns proxy for asserting underscore methods
+        var underscoreProxy = function () {
+            var self = this;
+            var proxy = {};
+            for(var name in _) {
+                var method = _[name];
+                if (typeof method != 'function') {
+                    continue;
+                }
+                (function(name, method){
+                    proxy[name] = function() {
+                        var _args = Array.prototype.slice.apply(arguments),
+                            args = [self.obj].concat(_args);
+
+                        self.assert(
+                            !!method.apply(_, args)
+                          , function(){ return 'expected ' + i(self.obj) + ' to ' + name + ' with args: ' + i(_args) }
+                          , function(){ return 'expected ' + i(self.obj) + ' to not ' + name + ' with args: ' + i(_args) });
+                    };
+                })(name, method)
+            }
+            return proxy;
+        };
+        var _Assertion = expect.Assertion;
+        var newAssertion = function (obj, flag, parent) {
+            _Assertion.apply(this, arguments);
+
+            // inject underscore proxy under 'to'
+            this.to._     = underscoreProxy.apply(this);
+            this.not.to._ = underscoreProxy.apply(this);
+        };
+
+        // create newExpect
+        var newExpect = function expect(obj) {
+            return new newAssertion(obj);
+        }
+
+        // copy all from expect --> newExpect
+        for (var name in expect) {
+            newExpect[name] = expect[name];
+        }
+        for (var name in expect.Assertion.prototype) {
+            newAssertion.prototype[name] = expect.Assertion.prototype[name];
+        }
+
+        return newExpect;
+    }
+};
+
